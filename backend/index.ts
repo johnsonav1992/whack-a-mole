04bc1d81109cdb7064dd8cb1e7be78187adcd672
Alex Johnson <johnsonav1992@gmail.com>
2024-07-25 06:08:08 -0500

    I'll get it working...

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,9 @@
-console.log('hello')
+import { Server } from "socket.io";
+
+const io = new Server(8080, {cors: { origin: "*" }, transports: ['websocket']});
+
+console.log('Socket.io server running on port 8080');
+
+io.on('connection', socket => {
+    console.log('a user connected - ' + socket.id);
+})
