8eb3e301e9c07efd1d1b20c7710450792fa15c97
Alex Johnson <johnsonav1992@gmail.com>
2024-08-15 05:35:32 -0500

    player action event type

--- backend/types/socketEventTypes.ts
+++ backend/types/socketEventTypes.ts
@@ -7,6 +7,7 @@ export type ClientToServerEvents = {
     'join-room': JoinOrLeaveRoomEvent;
     'leave-room': JoinOrLeaveRoomEvent;
     'level-select-initiated': LevelSelectInitiatedEvent;
+    'player-action': PlayerActionEvent;
 };
 
 export type ServerToClientEvents = Partial<{
@@ -19,9 +20,18 @@ export type ServerToClientEvents = Partial<{
     'leave-room': JoinOrLeaveRoomEvent;
     'user-leave': ( params: { userName: string } ) => void;
     'level-select-initiated': LevelSelectInitiatedEvent;
+    'player-action': PlayerActionEvent;
 }>;
 
 type WhackEvent = ( params: { moleIndex: number; playerId: number } ) => void;
 type SignInEvent = ( params: { playerUsername: string } ) => void;
-export type JoinOrLeaveRoomEvent = ( params: { room: string, userName: string } ) => void;
 type LevelSelectInitiatedEvent = (params: { roomName: string }) => void;
+
+export type PlayerActionEvent = (
+    params: { 
+        roomName: string; 
+        playerName: string; 
+        actionPayload: Record<string, unknown>
+    } 
+) => void;
+export type JoinOrLeaveRoomEvent = ( params: { room: string, userName: string } ) => void;
